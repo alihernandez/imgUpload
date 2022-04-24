b5c00ae2b414233c8cb6963aac997b3dcf41bbdc
phantomramen <ali.hernandez9two@gmail.com>
2022-04-24 17:26:48 -0600

    basic product

---
+++ config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+    url: "mongodb://localhost:27017/",
+    // url: "mongodb+srv://ahernDB:DB80808@gcluster.jg8tz.mongodb.net/gCluster?retryWrites=true&w=majority",
+    database: "gnoel",
+    imgBucket: "photos",
+  };
